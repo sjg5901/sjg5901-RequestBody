e758835906539271231cafc0f161dff369b02416
Skyler Giannetti <sjg5901@psu.edu>
2023-07-31 21:06:37 +0000

    Request Body lab complete

--- src/main/java/JavalinSingleton.java
+++ src/main/java/JavalinSingleton.java
@@ -1,5 +1,7 @@
 
 
+import org.eclipse.jetty.server.Authentication.User;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.javalin.Javalin;
 
@@ -23,7 +25,12 @@ public class JavalinSingleton {
         app.post("/echo", ctx -> {
             
             //implement logic here
-                
+            String jsonString = ctx.body();
+            
+            Song song = om.readValue(jsonString, Song.class);
+            ctx.json(song);
+
+            
         });
 
         /**
@@ -36,7 +43,13 @@ public class JavalinSingleton {
         app.post("/changeartisttobeatles", ctx -> {
 
             //implement logic here
-               
+            String jsonString = ctx.body();
+
+            Song song = om.readValue(jsonString, Song.class);
+
+            song.setArtistName("Beatles");
+
+            ctx.json(song);
         });
 
 
